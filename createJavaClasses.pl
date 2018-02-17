b068cf42cc7954a5d1fe758b64f30caf7600bff5
LKress <luis.kress@stud.th-bingen.de>
2018-02-17 23:07:18 +0100

    added methods/ better style

--- createJavaClasses.pl
+++ createJavaClasses.pl
@@ -8,13 +8,18 @@ print "Abstract?(y/n): ";
 chomp ($boolAbstr = <>);
 $boolAbstr = $boolAbstr eq "y"? " abstract class" : "";
 
-$boolInter = "";
+$boolInter = " class";
+$extends = "";
+$implements = "";
+$extends = "";
+$main = "";
+$boolMethoden = "";
+
 if($boolAbstr eq ""){
     print "Interface?(y/n): ";
     chomp ($boolInter = <>);
-    $boolInter = $boolInter eq "y"? " abstract class" : "";
+    $boolInter = $boolInter eq "y"? " interface" : " class";
 }
-
 
 $abstrOrInter = $boolAbstr . $boolInter;
 
@@ -31,46 +36,50 @@ if($boolExtends eq "y"){
     $possibleAbstract =~ s/.java//g;
     print "$possibleAbstract\n";
     print "Auswahl?: ";
-    chomp ($extends = <>);
-    $extends = "extends " . $extends;
-}
-else{
-  $extends = "";
+    chomp ($extends = "extends " . <>);
 }
 
-if($boolInter eq ""){
+if($boolInter eq " class"){
     print "Implements?(y/n): ";
     chomp ($boolImplements = <>);
     if($boolImplements eq "y"){
         #moegliche Interfaces finden
         while($programs =~ m/([A-Z](.*)\.java)/g){
             $tmp = `cat $1`;
-            if($tmp =~ m/interface ([A-Z]([a-z]|[0-9])*)/g){
-                print "$1 ";
-            }
+            print "$1 " if($tmp =~ m/interface ([A-Z]([a-z]|[0-9])*)/g);
         }
-        print "\n";
-        print "Auswahl?: ";
-        chomp ($implements = <>);
-        $implements = "implements " . $implements;
+        print "\nAuswahl?: ";
+        chomp ($implements = "implements " . <>);
     }
-    else{
-        $implements = "";
-    }
-}
-else{
-    $implements = "";
 }
 
-if($boolInter eq ""){
+if($boolInter eq " class"){
     print "Main?(y/n): ";
     chomp ($main = <>);
     $main = $main eq "y"? "public static void main(String[] args)\{\n\t\}" : "";
 }
-else{
-    $main = "";
-}
 
 system ("touch $className\.java");
 
-system ("echo 'public$abstrOrInter $className $extends $implements\{\n\t$main\n\}' >> $className\.java");
+system ("echo 'public$abstrOrInter $className $extends $implements\{\n\t$main' >> $className\.java");
+
+$on = 1;
+
+if($boolInter eq " class"){
+    print "Weitere Methoden?(y/n): ";
+    chomp ($boolMethoden = <>);
+    if($boolMethoden eq "y"){
+        while($on){
+            print "Public?(y/n): ";
+            chomp ($public = <>);
+            if($public eq "y"){
+                system ("echo '\n\tpublic' >> $className\.java");
+            }
+        }
+        system ("echo '\n\}' >> $className\.java");
+    }
+    else{
+      system ("echo '\n\}' >> $className\.java");
+      exit 1;
+    }
+}
